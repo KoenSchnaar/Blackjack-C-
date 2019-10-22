edd4369d26aef045146ab418a529f6688435acab
KoenSchnaar <koenschnaar@gmail.com>
2019-10-22 16:07:12 +0200

    deck kaarten in aparte class

---
+++ Blackjack/DeckKaarten.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blackjack.Extensions;
+
+namespace Blackjack
+{
+    class DeckKaarten
+    {
+        private List<Kaart> kaarten = new List<Kaart>();
+              
+
+        public List<Kaart> KaartspelMaken()
+        {
+            int[] nummers = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            string[] kleuren = new string[] { "Harten", "Schoppen", "Ruiten", "Klaver" };
+
+
+            for (int x = 0; x < nummers.Length; x++)
+            {
+                for (int y = 0; y < kleuren.Length; y++)
+                {
+                    kaarten.Add(new Kaart(nummers[x], kleuren[y]));
+                }
+            }
+
+            kaarten = kaarten.Shuffle();
+
+            return kaarten;
+
+        }
+
+    }
+}
--- Blackjack/Spel.cs
+++ Blackjack/Spel.cs
@@ -10,12 +10,12 @@ namespace Blackjack
     class Spel
     {
         private Speler speler = new Speler();
-        private List<Kaart> kaarten = new List<Kaart>();
+        private DeckKaarten deck = new DeckKaarten();
 
         public void SpelStarten()
         {
-            KaartspelMaken();
-            kaarten = kaarten.Shuffle();
+            var kaarten = deck.KaartspelMaken();
+            
             for (int i = 0; i < 52; i++)
             {
                 speler.Waarde += kaarten[i].Nummer;
@@ -25,23 +25,6 @@ namespace Blackjack
             }
             
 
-        }
-        
-        
-        public void KaartspelMaken()
-        {
-            int[] nummers = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-            string[] kleuren = new string[] { "Harten", "Schoppen", "Ruiten", "Klaver" };
-
-
-            for (int x = 0; x < nummers.Length; x++)
-            {
-                for (int y = 0; y < kleuren.Length; y++)
-                {
-                    kaarten.Add(new Kaart(nummers[x], kleuren[y]));
-                }
-            }
-            
         }
     }
 }
